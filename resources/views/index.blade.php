04b01bdc81f33e7a9764f5d9960b1ce44e89ba47
Elliot Taylor <elliot@raison.co>
2023-08-02 11:41:36 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,7 +17,7 @@
 @section('content')
 
     @if(session('saved'))
-        <div class="rounded-md bg-green-50">
+        <div class="rounded-md bg-green-50 p-4">
             <div class="flex">
                 <div class="flex-shrink-0">
                     <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -27,21 +27,9 @@
                     </svg>
                 </div>
                 <div class="ml-3">
-                    <h3 class="text-sm font-medium text-green-800">Post Saved</h3>
+                    <h3 class="text-sm font-medium text-green-800">Post Published</h3>
                     <div class="mt-2 text-sm text-green-700">
-                        <p>Saved post: {{$saved['slug']}}</p>
-                    </div>
-                    <div class="mt-4">
-                        <div class="-mx-2 -my-1.5 flex">
-                            <button type="button"
-                                    class="rounded-md bg-green-50 px-2 py-1.5 text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50">
-                                View status
-                            </button>
-                            <button type="button"
-                                    class="ml-3 rounded-md bg-green-50 px-2 py-1.5 text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50">
-                                Dismiss
-                            </button>
-                        </div>
+                        <p>You just saved this file ({{$saved['slug']}}) to be displayed on your blog.</p>
                     </div>
                 </div>
             </div>
