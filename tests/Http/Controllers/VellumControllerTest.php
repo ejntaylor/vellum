12bdf962f9b294d47a9457318a02e5df1c0905b9
ejntaylor <ejntaylor@users.noreply.github.com>
2023-08-14 11:08:00 +0000

    Fix styling

--- tests/Http/Controllers/VellumControllerTest.php
+++ tests/Http/Controllers/VellumControllerTest.php
@@ -1,8 +1,5 @@
 <?php
 
-use Ejntaylor\Vellum\Http\Controllers\VellumController;
-use Illuminate\Support\Facades\Auth;
-
 test('confirm environment is set to testing', function () {
     expect(config('app.env'))->toBe('testing');
 });
